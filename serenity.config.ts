fcc7609634208c8269130bbfc0262406114ca4c4
dennysvmosquera <dennysvmosquera@gmail.com>
2026-02-24 21:35:49 -0500

    serenity reporters config

---
+++ serenity.config.ts
@@ -0,0 +1,10 @@
+import { configure } from '@serenity-js/core';
+import { ConsoleReporter } from '@serenity-js/console-reporter';
+import { SerenityBDDReporter } from '@serenity-js/serenity-bdd';
+
+configure({
+    crew: [
+        ConsoleReporter.forDarkTerminals(),
+        new SerenityBDDReporter()
+    ],
+});
